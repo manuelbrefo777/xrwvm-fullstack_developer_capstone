11b101f28aaa03ec934c165fe3c2e7e17a26a707
manuelbrefo777 <brefo006@mail.com>
2026-02-22 14:47:01 -0500

    Added About Us page and configured routing

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,7 +1,8 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView # Ensure this import exists
 # from . import views
 
 app_name = 'djangoapp'
@@ -14,5 +15,7 @@ urlpatterns = [
     # path for dealer reviews view
 
     # path for add a review view
+    # ... other paths
+    path('about/', TemplateView.as_view(template_name="About.html")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
